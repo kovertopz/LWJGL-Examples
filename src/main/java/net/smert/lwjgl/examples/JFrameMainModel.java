c9044a00bf2845455356047cd5eb4b9551261708
Jason Sorensen <git@smert.net>
2014-09-07 23:45:56 -0400

    Created jFrame to display all main methods inside the JAR.

---
+++ src/main/java/net/smert/lwjgl/examples/JFrameMainModel.java
@@ -0,0 +1,152 @@
+package net.smert.lwjgl.examples;
+
+import com.jdotsoft.jarloader.JarClassLoader;
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.CodeSource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import javax.swing.AbstractListModel;
+import javax.swing.DefaultListModel;
+
+/**
+ *
+ * @author Jason Sorensen <sorensenj@smert.net>
+ */
+public class JFrameMainModel {
+
+    private final static String DIRECTORY_PREFIX = "net/smert/lwjgl/examples/";
+    private final static String PACKAGE_PREFIX = "net.smert.lwjgl.examples.";
+
+    private final DefaultListModel listModelMainClasses;
+    private final JarClassLoader jarClassLoader;
+
+    public JFrameMainModel() {
+        jarClassLoader = new JarClassLoader();
+        listModelMainClasses = new DefaultListModel();
+    }
+
+    private String getFileExtension(String filename) {
+        int index = filename.lastIndexOf('.');
+        String extension = "";
+
+        if (index > 0) {
+            extension = filename.substring(index + 1);
+        }
+
+        return extension;
+    }
+
+    private String getFileNameWithoutExtension(String filename) {
+        int index = filename.lastIndexOf('.');
+        String name = "";
+
+        if (index > 0) {
+            name = filename.substring(0, index);
+        }
+
+        return name;
+    }
+
+    private List<String> getMainClasses() {
+        CodeSource src = Main.class.getProtectionDomain().getCodeSource();
+        List<String> mainClasses = new ArrayList();
+
+        if (src != null) {
+            URL jar = src.getLocation();
+            ZipInputStream zip;
+
+            try {
+                zip = new ZipInputStream(jar.openStream());
+
+                while (true) {
+                    ZipEntry e = zip.getNextEntry();
+
+                    if (e == null) {
+                        break;
+                    }
+
+                    String fullpath = e.getName();
+
+                    if (fullpath.startsWith(DIRECTORY_PREFIX)) {
+                        String extension = getFileExtension(fullpath);
+
+                        if (!extension.startsWith("class")) {
+                            continue;
+                        }
+
+                        Path path = Paths.get(fullpath);
+                        String filename = path.getFileName().toString();
+
+                        if (filename.contains("$")) {
+                            continue;
+                        }
+
+                        String filenameWithoutExtension = getFileNameWithoutExtension(filename);
+
+                        if (filenameWithoutExtension.equals(Main.class.getSimpleName())) {
+                            continue;
+                        }
+
+                        try {
+                            Class clazz = jarClassLoader.loadClass(PACKAGE_PREFIX + filenameWithoutExtension);
+                            Method method = clazz.getMethod("main", new Class[]{String[].class});
+
+                            boolean validModifiers = false;
+                            boolean validVoid = false;
+
+                            if (method != null) {
+                                method.setAccessible(true);
+                                int nModifiers = method.getModifiers();
+                                validModifiers = Modifier.isPublic(nModifiers) && Modifier.isStatic(nModifiers);
+                                Class<?> clazzRet = method.getReturnType();
+                                validVoid = (clazzRet == void.class);
+                            }
+                            if (method == null || !validModifiers || !validVoid) {
+                                continue;
+                            }
+
+                            mainClasses.add(PACKAGE_PREFIX + filenameWithoutExtension);
+                        } catch (ClassNotFoundException cnfe) {
+                            cnfe.printStackTrace();
+                        } catch (NoSuchMethodException | SecurityException ex) {
+                            // Do nothing since not all classes have main methods.
+                        }
+                    }
+                }
+            } catch (IOException ie) {
+                ie.printStackTrace();
+            }
+        }
+
+        return mainClasses;
+    }
+
+    public AbstractListModel populateListModel() {
+        List<String> mainClasses = getMainClasses();
+
+        for (String clazz : mainClasses) {
+            listModelMainClasses.addElement(clazz);
+        }
+
+        return listModelMainClasses;
+    }
+
+    public void runDemo(String mainClass, String[] args) {
+        try {
+            jarClassLoader.invokeMain(mainClass, args);
+        } catch (Throwable e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+
+}
---
+++ src/main/java/net/smert/lwjgl/examples/Main.java
@@ -0,0 +1,73 @@
+package net.smert.lwjgl.examples;
+
+import java.io.IOException;
+import java.lang.reflect.Method;
+import java.lang.reflect.Modifier;
+import java.net.URL;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.CodeSource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Jason Sorensen <sorensenj@smert.net>
+ */
+public class Main {
+
+    public final static int WIDTH = 600;
+    public final static int HEIGHT = 300;
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(final String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(JFrameMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(JFrameMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(JFrameMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(JFrameMain.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frameMain = new JFrameMain(args);
+                frameMain.setVisible(true);
+                frameMain.setBounds((GetScreenWorkingWidth() - WIDTH) / 2, (GetScreenWorkingHeight() - HEIGHT) / 2, WIDTH, HEIGHT);
+            }
+        });
+    }
+
+    public static int GetScreenWorkingHeight() {
+        return java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().height;
+    }
+
+    public static int GetScreenWorkingWidth() {
+        return java.awt.GraphicsEnvironment.getLocalGraphicsEnvironment().getMaximumWindowBounds().width;
+    }
+
+}
