4755d580af8d3f6d185ee6934a3932905fbf02c5
Jason Sorensen <git@smert.net>
2014-09-16 02:18:50 -0400

    Remove unused import.

--- src/main/java/net/smert/lwjgl/examples/JFrameMainModel.java
+++ src/main/java/net/smert/lwjgl/examples/JFrameMainModel.java
@@ -2,7 +2,6 @@ package net.smert.lwjgl.examples;
 
 import com.jdotsoft.jarloader.JarClassLoader;
 import java.io.File;
-import java.io.FilenameFilter;
 import java.io.IOException;
 import java.lang.reflect.Method;
 import java.lang.reflect.Modifier;
